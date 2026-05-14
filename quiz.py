b080b250e346f2de9ae72ca1c39737e15fdca6d6
LakmalPerera <lakmal.perera.it@gmail.com>
2026-05-14 18:06:16 +0530

    Build upper hull conditions

--- quiz.py
+++ quiz.py
@@ -27,4 +27,8 @@ def convex_hull(points):
                 lower.append(p)
         
         upper = []
-        
+
+        for p in reversed(points):
+        while len(upper) >= 2 and cross(upper[-2], upper[-1], p) <= 0:
+            upper.pop()
+        upper.append(p)
